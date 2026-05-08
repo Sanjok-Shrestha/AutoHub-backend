e37c90ec4883d63e33e7eb41c5fd531490acacf5
sadiksha4 <sadiksha1357@gmail.com>
2026-05-08 13:58:06 +0545

    Added Reports backend API Sadiksha

---
+++ AutoHub/VehiclePartsAPI/DTOs/VendorSpendDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleManagementSystem.VehiclePartsAPI.DTOs
+{
+    public class VendorSpendDto
+    {
+        public string VendorName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
